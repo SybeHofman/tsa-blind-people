e920672d2a5665d362f13aea1c615823903ea7a2
Sybe Hofman <sybe.hofman@outlook.com>
2025-09-26 22:07:06 -0700

    Finished basic signup backend

--- client/src/Components/App.tsx
+++ client/src/Components/App.tsx
@@ -1,9 +1,17 @@
 import "./App.css";
 import Login from "./Login/Login.tsx";
+import Signup from "./Signup/Signup.tsx";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
 
 function App() {
   return (
-    <Login></Login>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
---
+++ client/src/Components/Signup/Signup.tsx
@@ -0,0 +1,59 @@
+import {Link} from "react-router-dom";
+import "./Signup.css";
+import { useRef } from "react";
+import type { MouseEvent } from "react";
+
+function Signup() {
+  const usernameRef = useRef<HTMLInputElement>(null);
+  const passwordRef = useRef<HTMLInputElement>(null);
+
+  const handleClick = (e: MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+    
+    if(usernameRef.current == null || passwordRef.current == null) {
+      console.error("Username or password input is null");
+      return;
+    }
+
+    if(usernameRef.current.value === "" || passwordRef.current.value === "") {
+      console.error("Username or password is empty");
+      return;
+    }
+    
+    postData(usernameRef.current?.value, passwordRef.current?.value);
+  }
+
+  const postData = async (username: string, password: string) => {
+    const list = {username: username, password: password}
+    const options = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(list)
+    }
+
+    const response = await fetch('/api', options);
+
+    const data = await response.json();
+    console.log(data);
+  }
+
+  return (
+    <>
+      <h1>Welcome to Beyond Eyes! Please signup with username and password</h1>
+      <div className = "signup">
+        <label className="signup-contents" htmlFor = "usernameInput">Username: </label>
+        <input ref={usernameRef} className="signup-contents" type="text" id="usernameInput" aria-label="Input one of two"></input><br/>
+
+        <label className="signup-contents" htmlFor = "passwordInput">Password: </label>
+        <input ref={passwordRef} className="signup-contents" type="password" id="passwordInput" aria-label="Input two of two"></input> <br/>
+
+        <button className="signup-contents submit-button" type="submit" onClick={handleClick}>Submit</button>
+      </div>
+      <Link to="/">Home</Link>
+    </>
+  )
+}
+
+export default Signup;
