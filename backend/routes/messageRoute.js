f1eda6d8ec7ffc71cf4de923a6f56701fbf71c5d
Sybe Hofman <sybe.hofman@outlook.com>
2025-10-15 19:52:46 -0700

    Finished with basic message sending and receiving functionality.
    TODO: Fix new logins not updating homepage

--- backend/routes/messageRoute.js
+++ backend/routes/messageRoute.js
@@ -24,41 +24,58 @@ router.post("/", async (request, response) => {
     timestamp: new Date(),
   }
   
-  response.json(newData);
+  database.insert(newData, (error) => {
+    if (error) {
+      console.error("Error inserting data into the database:", error);
+      return response.status(500).send("Error saving message to the database");
+    }
+    console.log(newData);
+    response.json(newData);
+  });
 })
 
 router.get("/from/:id", (request, response) => {
-  const from = request.params.id;
-  database.find({from: id}, (error, data) => {
-    if(error) {
-      console.log("Error retrieving from messages:", error);
-      return response.status(500).send("Error retrieving messages");
-    }
+  try{
+    const from = request.params.id;
+    database.find({from: from}, (error, data) => {
+      if(error) {
+        console.log("Error retrieving from messages:", error);
+        return response.status(500).send("Error retrieving messages");
+      }
 
-    if(!data) {
-      console.log("No from messages found from user:", from);
-      return response.status(404).send("No messages found from this user");
-    }
+      if(!data) {
+        console.log("No from messages found from user:", from);
+        return response.status(404).send("No messages found from this user");
+      }
 
-    response.json(data);
-  })
+      response.json(data);
+    })
+  } catch (error) {
+    console.log(error);
+    return response.status(500).send("Error retrieving messages");
+  }
 })
 
 router.get("/to/:id", (request, response) => {
   const to = request.params.id;
-  database.find({to: id}, (error, data) => {
-    if(error) {
-      console.log("Error retrieving to messages:", error);
-      return response.status(500).send("Error retrieving messages");
-    }
+  try{
+    database.find({to: to}, (error, data) => {
+      if(error) {
+        console.log("Error retrieving to messages:", error);
+        return response.status(500).send("Error retrieving messages");
+      }
 
-    if(!data) {
-      console.log("No to messages found from user:", from);
-      return response.status(404).send("No messages found from this user");
-    }
+      if(!data) {
+        console.log("No to messages found from user:", from);
+        return response.status(404).send("No messages found from this user");
+      }
 
-    response.json(data);
-  })
+      response.json(data);
+    })
+  } catch (error) {
+    console.log(error);
+    return response.status(500).send("Error retrieving messages");
+  }
 });
 
 module.exports = router;
--- client/src/Components/Homepage/Homepage.tsx
+++ client/src/Components/Homepage/Homepage.tsx
@@ -2,20 +2,47 @@ import {Link} from "react-router-dom";
 import { useState, useEffect, useRef } from "react";
 
 function Homepage() {
-  const [messages, setMessages] = useState([{body: "Loading messages..."}]);
+  const [fromMessages, setFromMessages] = useState([{content: "Loading messages..."}]);
+  const [toMessages, setToMessages] = useState([{content: "Loading messages..."}]);
   const [username, setUsername] = useState("Guest");
-  const _id = localStorage.getItem("_id") || sessionStorage.getItem("_id");
+  const _id = sessionStorage.getItem("_id")?.replaceAll("\"", ""); //Remove quotes from id
   const messageRef = useRef<HTMLInputElement>(null);
 
-  const fetchMessages = async () => {
+  //Gets messages made by the user
+  const fetchFromMessages = async () => {
     try{
-      const response = await fetch(`/api/users/messages/${_id}`);
+      const response = await fetch(`/api/messages/from/${_id}`);
       if(!response.ok) {
         console.log("Failed to fetch messages");
         return;
       }
 
-      setMessages(await response.json());
+      const responseJson = await response.json();
+      if(responseJson.length === 0) {
+        setFromMessages([{content: "No sent messages"}]);
+      } else{
+        setFromMessages(responseJson);
+      }
+    } catch(error) {
+      console.log("Error fetching messages:", error);
+    }
+  }
+
+  //Gets messages sent to the user
+  const fetchToMessages = async () => {
+    try{
+      const response = await fetch(`/api/messages/to/${_id}`);
+      if(!response.ok) {
+        console.log("Failed to fetch messages");
+        return;
+      }
+
+      const responseJson = await response.json();
+      if(responseJson.length === 0) {
+        setToMessages([{content: "No recieved messages"}]);
+      } else{
+        setToMessages(responseJson);
+      }
     } catch(error) {
       console.log("Error fetching messages:", error);
     }
@@ -34,22 +61,23 @@ function Homepage() {
   }
 
   useEffect(() => {
-    fetchMessages();
+    fetchFromMessages();
+    fetchToMessages();
   }, [_id])
 
-  const putMessage = async () => {
-    const newMessage = messageRef.current?.value;
+  const postMessage = async () => {
+    const content = messageRef.current?.value;
 
     const options = {
-      method: "PUT",
+      method: "POST",
       headers: {
         "Content-Type": "application/json"
       },
 
-      body: JSON.stringify({fromUserId: _id, toUserId: await fetchId("Tygo"), newMessage})
+      body: JSON.stringify({from: _id, to: await fetchId("Tygo"), content: content})
     }
 
-    const response = await fetch("/api/users/messages", options);
+    const response = await fetch("/api/messages", options);
     if(!response.ok) {
       console.log("Failed to post message");
       return;
@@ -57,7 +85,8 @@ function Homepage() {
 
     console.log(JSON.stringify(response.body));
 
-    fetchMessages();
+    fetchFromMessages();
+    fetchToMessages();
 
     return response.json();
   }
@@ -65,14 +94,34 @@ function Homepage() {
   return (
     <>
       <Link to="/Login">Login</Link>
-      {messages.map((message, index) => (
-        <div className="message"key={index}>{message.body}</div>
-      ))}
-      
-      <div className = "message-input">
-        <input ref={messageRef} type = "text" id="messageInput" aria-label="Input message"></input>
-        <button type="submit" onClick={putMessage}>Submit</button>
-      </div>
+      {
+        _id === null ?
+        //Not logged in
+        <>
+          <h1>Welcome to beyond eyes!</h1>
+        </> :
+        //Logged in
+        <>
+          <div className = "from-messages">
+            Your sent messages:
+            {fromMessages.map((message, index) => (
+              <div className="message"key={index}>{message.content}</div>
+            ))}
+          </div>
+
+          <div className = "to-messages">
+            Your recieved messages:
+            {toMessages.map((message, index) => (
+              <div className="message"key={index}>{message.content}</div>
+            ))}
+          </div>
+          
+          <div className = "message-input">
+            <input ref={messageRef} type = "text" id="messageInput" aria-label="Input message"></input>
+            <button type="submit" onClick={postMessage}>Submit</button>
+          </div>
+        </>
+      }
     </>
   )
 }
